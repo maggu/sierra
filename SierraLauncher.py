b60e545ddcc3f092e598d440a510bb121eef052c
C C Magnus Gustavsson <maggu@users.noreply.github.com>
2020-02-02 20:02:00 +0100

    Use Python 3 and GTK+ 3

--- SierraLauncher.py
+++ SierraLauncher.py
@@ -1,31 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-# Sierra Launcher replacement by C C Magnus Gustavsson
+'''Sierra Launcher replacement by C C Magnus Gustavsson'''
 
-import os, pygtk
-pygtk.require('2.0')
-
-from gtk import *
+import gi, os
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from gi.repository.Gtk import AttachOptions as options
 
 # Actions for Linux/Unix (instead of SierraLauncher.ini)
-actions = ["acroread Manuals/KQManual.pdf &",
-           "dosbox kq1sci/SIERRA.COM -fullscreen -exit &",
-           "dosbox kq2/SIERRA.COM -fullscreen -exit &",
-           "dosbox kq3/SIERRA.COM -fullscreen -exit &",
-           "dosbox kq4/SIERRA.COM -fullscreen -exit &",
-           "dosbox kq5/SIERRA.EXE -fullscreen -exit &",
-           "dosbox kq6/SIERRA.EXE -fullscreen -exit &",
-           "wine kq7/SIERRAW.EXE kq7/RESOURCE.WIN &"]
+ACTIONS = [
+    "acroread Manuals/KQManual.pdf &",
+    "dosbox kq1sci/SIERRA.COM -fullscreen -exit &",
+    "dosbox kq2/SIERRA.COM -fullscreen -exit &",
+    "dosbox kq3/SIERRA.COM -fullscreen -exit &",
+    "dosbox kq4/SIERRA.COM -fullscreen -exit &",
+    "dosbox kq5/SIERRA.EXE -fullscreen -exit &",
+    "dosbox kq6/SIERRA.EXE -fullscreen -exit &",
+    "wine kq7/SIERRAW.EXE kq7/RESOURCE.WIN &"
+]
 
-class SierraLauncher:
+class SierraLauncher(object):
     def destroy(self, widget, data=None):
-        main_quit()
+        Gtk.main_quit()
 
     def delete_event(self, widget, event, data=None):
         self.destroy(self, widget)
 
     def launch(self, widget, data=None):
-        os.system(actions[widget.num])
+        os.system(ACTIONS[widget.num])
         if self.close_window_on_launch:
             self.destroy(self, widget)
 
@@ -35,61 +37,61 @@ class SierraLauncher:
     def __init__(self):
         self.close_window_on_launch = False
 
-        self.window = Window(WINDOW_TOPLEVEL)
+        self.window = Gtk.Window()
         self.window.set_title("King's Quest Collection(TM)")
         self.window.set_icon_from_file("Sierra.ico")
         self.window.set_resizable(False)
         self.window.connect("destroy", self.destroy)
         self.window.connect("delete_event", self.delete_event)
 
-        self.table = Table(columns=3, rows=9, homogeneous=False)
+        self.table = Gtk.Table(columns=3, rows=9, homogeneous=False)
         self.table.set_border_width(8)
         self.window.add(self.table)
 
         for i in range(1, 8):
-            self.button = Button('               Launch               ')
+            self.button = Gtk.Button('{:^36}'.format('Launch'))
             self.button.num = i
             self.button.connect("clicked", self.launch, None)
             self.button.set_border_width(4)
             self.button.show()
 
-            self.frame = Frame("King's Quest " + str(i))
+            self.frame = Gtk.Frame(label="King's Quest {:d}".format(i))
             self.frame.add(self.button)
-            self.table.attach(self.frame, 0, 2, i-1, i, xoptions=FILL,
-                              yoptions=FILL, xpadding=8, ypadding=2)
+            self.table.attach(self.frame, 0, 2, i-1, i, xoptions=options.FILL,
+                              yoptions=options.FILL, xpadding=8, ypadding=2)
             self.frame.show()
 
-        self.button = Button('View Manual')
+        self.button = Gtk.Button('View Manual')
         self.button.num = 0
         self.button.connect("clicked", self.launch, None)
-        self.table.attach(self.button, 0, 1, 7, 8, xoptions=EXPAND,
-                          yoptions=EXPAND, xpadding=4, ypadding=4)
+        self.table.attach(self.button, 0, 1, 7, 8, xoptions=options.EXPAND,
+                          yoptions=options.EXPAND, xpadding=4, ypadding=4)
         self.button.show()
 
-        self.button = Button('     Close     ')
-        self.button.connect_object("clicked", Widget.destroy, self.window)
-        self.table.attach(self.button, 1, 2, 7, 8, xoptions=EXPAND,
-                          yoptions=EXPAND, xpadding=4, ypadding=4)
+        self.button = Gtk.Button('{:^15}'.format('Close'))
+        self.button.connect_object("clicked", Gtk.Widget.destroy, self.window)
+        self.table.attach(self.button, 1, 2, 7, 8, xoptions=options.EXPAND,
+                          yoptions=options.EXPAND, xpadding=4, ypadding=4)
         self.button.show()
 
-        self.button = CheckButton("Close window on launch")
+        self.button = Gtk.CheckButton("Close window on launch")
         self.button.set_active(self.close_window_on_launch)
         self.button.connect("toggled", self.toggle, None)
-        self.table.attach(self.button, 0, 2, 8, 9, xoptions=EXPAND,
-                          yoptions=EXPAND, xpadding=4, ypadding=4)
+        self.table.attach(self.button, 0, 2, 8, 9, xoptions=options.EXPAND,
+                          yoptions=options.EXPAND, xpadding=4, ypadding=4)
         self.button.show()
 
-        self.image = Image()
+        self.image = Gtk.Image()
         self.image.set_from_file("gameart.bmp")
-        self.table.attach(self.image, 2, 3, 0, 9, xoptions=EXPAND,
-                          yoptions=EXPAND, xpadding=8, ypadding=8)
+        self.table.attach(self.image, 2, 3, 0, 9, xoptions=options.EXPAND,
+                          yoptions=options.EXPAND, xpadding=8, ypadding=8)
         self.image.show()
 
         self.table.show()
         self.window.show()
 
     def main(self):
-        main()
+        Gtk.main()
 
 if __name__ == "__main__":
     SierraLauncher().main()
--- SierraLauncherSC.py
+++ SierraLauncherSC.py
@@ -1,37 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-# Sierra Launcher replacement by C C Magnus Gustavsson
+'''Sierra Launcher replacement by C C Magnus Gustavsson'''
 
-import os, pygtk
-pygtk.require('2.0')
-
-from gtk import *
+import gi, os
+gi.require_version('Gtk', '3.0')
+from gi.repository import GdkPixbuf, Gtk
+from gi.repository.GdkPixbuf import InterpType as interp_type
+from gi.repository.Gtk import AttachOptions as options
 
 # Actions for Linux/Unix (instead of SierraLauncher.ini)
-actions = ["acroread kq/Manuals/KQManual.pdf &",
-           "dosbox kq/kq1sci/SIERRA.COM -fullscreen -exit &",
-           "dosbox kq/kq2/SIERRA.COM -fullscreen -exit &",
-           "dosbox kq/kq3/SIERRA.COM -fullscreen -exit &",
-           "dosbox kq/kq4/SIERRA.COM -fullscreen -exit &",
-           "dosbox kq/kq5/SIERRA.EXE -fullscreen -exit &",
-           "dosbox kq/kq6/SIERRA.EXE -fullscreen -exit &",
-           "wine kq/kq7/SIERRAW.EXE kq/kq7/RESOURCE.WIN &",
-           "acroread lsl/Manuals/LarryManual.pdf &",
-           "dosbox lsl/lsl1vga/SCIDHUV.EXE -fullscreen -exit &",
-           "dosbox lsl/lsl2/SIERRA.COM -fullscreen -exit &",
-           "dosbox lsl/lsl3/SIERRA.COM -fullscreen -exit &",
-           "dosbox lsl/lsl5/SCIDHUV.EXE -fullscreen -exit &",
-           "dosbox lsl/lsl6/SIERRA.EXE -fullscreen -exit &"]
+ACTIONS = [
+    "acroread kq/Manuals/KQManual.pdf &",
+    "dosbox kq/kq1sci/SIERRA.COM -fullscreen -exit &",
+    "dosbox kq/kq2/SIERRA.COM -fullscreen -exit &",
+    "dosbox kq/kq3/SIERRA.COM -fullscreen -exit &",
+    "dosbox kq/kq4/SIERRA.COM -fullscreen -exit &",
+    "dosbox kq/kq5/SIERRA.EXE -fullscreen -exit &",
+    "dosbox kq/kq6/SIERRA.EXE -fullscreen -exit &",
+    "wine kq/kq7/SIERRAW.EXE kq/kq7/RESOURCE.WIN &",
+    "acroread lsl/Manuals/LarryManual.pdf &",
+    "dosbox lsl/lsl1vga/SCIDHUV.EXE -fullscreen -exit &",
+    "dosbox lsl/lsl2/SIERRA.COM -fullscreen -exit &",
+    "dosbox lsl/lsl3/SIERRA.COM -fullscreen -exit &",
+    "dosbox lsl/lsl5/SCIDHUV.EXE -fullscreen -exit &",
+    "dosbox lsl/lsl6/SIERRA.EXE -fullscreen -exit &"
+]
 
-class SierraLauncher:
+class SierraLauncher(object):
     def destroy(self, widget, data=None):
-        main_quit()
+        Gtk.main_quit()
 
     def delete_event(self, widget, event, data=None):
         self.destroy(self, widget)
 
     def launch(self, widget, data=None):
-        os.system(actions[widget.num])
+        os.system(ACTIONS[widget.num])
         if self.close_window_on_launch:
             self.destroy(self, widget)
 
@@ -41,104 +44,102 @@ class SierraLauncher:
     def __init__(self):
         self.close_window_on_launch = False
 
-        self.window = Window(WINDOW_TOPLEVEL)
+        self.window = Gtk.Window()
         self.window.set_title("Sierra Collections")
         self.window.set_icon_from_file("kq/Sierra.ico")
         self.window.set_resizable(False)
         self.window.connect("destroy", self.destroy)
         self.window.connect("delete_event", self.delete_event)
 
-        self.table = Table(columns=5, rows=9, homogeneous=False)
+        self.table = Gtk.Table(columns=5, rows=9, homogeneous=False)
         self.table.set_border_width(8)
         self.window.add(self.table)
 
         for i in range(1, 8):
-            self.button = Button('               Launch               ')
+            self.button = Gtk.Button('{:^36}'.format('Launch'))
             self.button.num = i
             self.button.connect("clicked", self.launch, None)
             self.button.set_border_width(4)
             self.button.show()
 
-            self.frame = Frame("King's Quest " + str(i))
+            self.frame = Gtk.Frame(label="King's Quest {:d}".format(i))
             self.frame.add(self.button)
-            self.table.attach(self.frame, 0, 1, i-1, i, xoptions=FILL,
-                              yoptions=FILL, xpadding=8, ypadding=2)
+            self.table.attach(self.frame, 0, 1, i-1, i, xoptions=options.FILL,
+                              yoptions=options.FILL, xpadding=8, ypadding=2)
             self.frame.show()
 
-        self.button = Button('View Manual')
+        self.button = Gtk.Button('View Manual')
         self.button.num = 0
         self.button.connect("clicked", self.launch, None)
-        self.table.attach(self.button, 0, 1, 7, 8, xoptions=EXPAND,
-                          yoptions=EXPAND, xpadding=4, ypadding=16)
+        self.table.attach(self.button, 0, 1, 7, 8, xoptions=options.EXPAND,
+                          yoptions=options.EXPAND, xpadding=4, ypadding=16)
         self.button.show()
 
         for i in range(1, 6):
-            self.button = Button('               Launch               ')
+            self.button = Gtk.Button('{:^36}'.format('Launch'))
             self.button.num = i+8
             self.button.connect("clicked", self.launch, None)
             self.button.set_border_width(4)
             self.button.show()
 
-            if i > 3:
-                self.frame = Frame("Leisure Suit Larry " + str(i+1))
-            else:
-                self.frame = Frame("Leisure Suit Larry " + str(i))
+            v = i + 1 if i > 3 else i
+            self.frame = Gtk.Frame(label="Leisure Suit Larry {:d}".format(v))
             self.frame.add(self.button)
-            self.table.attach(self.frame, 4, 5, i-1, i, xoptions=FILL,
-                              yoptions=FILL, xpadding=8, ypadding=2)
+            self.table.attach(self.frame, 4, 5, i-1, i, xoptions=options.FILL,
+                              yoptions=options.FILL, xpadding=8, ypadding=2)
             self.frame.show()
 
-        self.button = Button('View Manual')
+        self.button = Gtk.Button('View Manual')
         self.button.num = 8
         self.button.connect("clicked", self.launch, None)
-        self.table.attach(self.button, 4, 5, 5, 6, xoptions=EXPAND,
-                          yoptions=EXPAND, xpadding=4, ypadding=4)
+        self.table.attach(self.button, 4, 5, 5, 6, xoptions=options.EXPAND,
+                          yoptions=options.EXPAND, xpadding=4, ypadding=4)
         self.button.show()
 
-        self.button = Button('     Close     ')
-        self.button.connect_object("clicked", Widget.destroy, self.window)
-        self.table.attach(self.button, 4, 5, 7, 8, xoptions=EXPAND,
-                          yoptions=EXPAND, xpadding=4, ypadding=4)
+        self.button = Gtk.Button('{:^15}'.format('Close'))
+        self.button.connect_object("clicked", Gtk.Widget.destroy, self.window)
+        self.table.attach(self.button, 4, 5, 7, 8, xoptions=options.EXPAND,
+                          yoptions=options.EXPAND, xpadding=4, ypadding=4)
         self.button.show()
 
-        self.button = CheckButton("Close window on launch")
+        self.button = Gtk.CheckButton("Close window on launch")
         self.button.set_active(self.close_window_on_launch)
         self.button.connect("toggled", self.toggle, None)
-        self.table.attach(self.button, 4, 5, 8, 9, xoptions=EXPAND,
+        self.table.attach(self.button, 4, 5, 8, 9, xoptions=options.EXPAND,
                           yoptions=0, xpadding=4, ypadding=4)
         self.button.show()
 
-        self.image = Image()
-        pixbuf = gdk.pixbuf_new_from_file("kq/gameart.bmp")
-        scaled_buf = pixbuf.scale_simple(200, 237, gdk.INTERP_BILINEAR)
+        self.image = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file("kq/gameart.bmp")
+        scaled_buf = pixbuf.scale_simple(200, 237, interp_type.BILINEAR)
         self.image.set_from_pixbuf(scaled_buf)
-        self.table.attach(self.image, 2, 3, 0, 4, xoptions=EXPAND,
-                          yoptions=EXPAND, xpadding=8, ypadding=4)
+        self.table.attach(self.image, 2, 3, 0, 4, xoptions=options.EXPAND,
+                          yoptions=options.EXPAND, xpadding=8, ypadding=4)
         self.image.show()
 
-        self.image = Image()
-        pixbuf = gdk.pixbuf_new_from_file("lsl/gameart.bmp")
-        scaled_buf = pixbuf.scale_simple(200, 237, gdk.INTERP_BILINEAR)
+        self.image = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file("lsl/gameart.bmp")
+        scaled_buf = pixbuf.scale_simple(200, 237, interp_type.BILINEAR)
         self.image.set_from_pixbuf(scaled_buf)
-        self.table.attach(self.image, 2, 3, 4, 9, xoptions=EXPAND,
-                          yoptions=EXPAND, xpadding=8, ypadding=2)
+        self.table.attach(self.image, 2, 3, 4, 9, xoptions=options.EXPAND,
+                          yoptions=options.EXPAND, xpadding=8, ypadding=2)
         self.image.show()
 
-        self.arrow = Arrow(ARROW_LEFT, SHADOW_OUT)
-        self.table.attach(self.arrow, 1, 2, 0, 1, xoptions=SHRINK,
-                          yoptions=SHRINK, xpadding=2, ypadding=2)
+        self.arrow = Gtk.Arrow(Gtk.ArrowType.LEFT, Gtk.ShadowType.OUT)
+        self.table.attach(self.arrow, 1, 2, 0, 1, xoptions=options.SHRINK,
+                          yoptions=options.SHRINK, xpadding=2, ypadding=2)
         self.arrow.show()
 
-        self.arrow = Arrow(ARROW_RIGHT, SHADOW_OUT)
-        self.table.attach(self.arrow, 3, 4, 4, 5, xoptions=SHRINK,
-                          yoptions=SHRINK, xpadding=2, ypadding=2)
+        self.arrow = Gtk.Arrow(Gtk.ArrowType.RIGHT, Gtk.ShadowType.OUT)
+        self.table.attach(self.arrow, 3, 4, 4, 5, xoptions=options.SHRINK,
+                          yoptions=options.SHRINK, xpadding=2, ypadding=2)
         self.arrow.show()
 
         self.table.show()
         self.window.show()
 
     def main(self):
-        main()
+        Gtk.main()
 
 if __name__ == "__main__":
     SierraLauncher().main()
